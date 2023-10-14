2d9b2c4df7a9c865734c6aa89a2665c005af0bd2
JaewookMun <mun1103.dev@gmail.com>
2023-10-15 00:41:51 +0900

    1. Modify    1) hide method in springdoc

--- src/main/java/com/sillimfive/mymap/web/dto/roadmap/RoadMapCreateDto.java
+++ src/main/java/com/sillimfive/mymap/web/dto/roadmap/RoadMapCreateDto.java
@@ -5,6 +5,7 @@ import com.sillimfive.mymap.domain.Image;
 import com.sillimfive.mymap.domain.User;
 import com.sillimfive.mymap.domain.roadmap.RoadMap;
 import com.sillimfive.mymap.domain.roadmap.RoadMapNode;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -30,11 +31,13 @@ public class RoadMapCreateDto {
     private List<Long> tagIds;
     private List<String> newTags;
 
+    @Schema(hidden = true)
     public RoadMap convert(User user, Category category, Image image) {
 
         return RoadMap.createRoadMap(user, category, title, description, image);
     }
 
+    @Schema(hidden = true)
     public List<RoadMapNode> getRoadMapNodesFromDto() {
         Collections.sort(nodeDtoList, Comparator.comparing(node -> Integer.valueOf(node.getOrder())));
 
